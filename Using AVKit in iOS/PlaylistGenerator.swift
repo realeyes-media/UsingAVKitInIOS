31d02f52351a90eaae9a90903ab7285a19dcb9d0
John G. Gainfort, Jr <john@realeyes.com>
2019-11-11 10:45:29 -0700

    added query params

--- Using AVKit in iOS/PlaylistGenerator.swift
+++ Using AVKit in iOS/PlaylistGenerator.swift
@@ -9,16 +9,16 @@
 import Foundation
 
 let mediaPlaylistVideoDict: [String: String] = [
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/350k_cmaf/350k_cmaf.m3u8" : mediaPlaylistVideo350k,
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/860k_cmaf/860k_cmaf.m3u8" : mediaPlaylistVideo860k,
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/1850k_cmaf/1850k_cmaf.m3u8" : mediaPlaylistVideo1850k,
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/3000k_cmaf/3000k_cmaf.m3u8" : mediaPlaylistVideo3000k,
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/4830k_cmaf/4830k_cmaf.m3u8" : mediaPlaylistVideo4830k
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/350k_cmaf/350k_cmaf.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9" : mediaPlaylistVideo350k,
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/860k_cmaf/860k_cmaf.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9" : mediaPlaylistVideo860k,
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/1850k_cmaf/1850k_cmaf.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9" : mediaPlaylistVideo1850k,
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/3000k_cmaf/3000k_cmaf.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9" : mediaPlaylistVideo3000k,
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/4830k_cmaf/4830k_cmaf.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9" : mediaPlaylistVideo4830k
 ]
 
 let mediaPlaylistAudioDict: [String: String] = [
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/layer_surround/layer_surround_cmaf-0.m3u8": mediaPlaylistAudioSurround,
-"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/layer_stereo/layer_stereo_cmaf-1.m3u8" : mediaPlaylistAudioStereo
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/layer_surround/layer_surround_cmaf-0.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9": mediaPlaylistAudioSurround,
+"https://llnw-stg.vod.peacocktv.com/pub/usterr/212/221/GMO_00000000002656_01/cmaf/layer_stereo/layer_stereo_cmaf-1.m3u8?e=1573260512&p=46&cd=1573224512&cf=1573224512&h=58e1b2b28bdd1c46042b7ea38d47a7d9" : mediaPlaylistAudioStereo
 ]
 
 func generatePlaylist(fromData data: Data, url: String, isMaster: Bool, isAudio: Bool) -> Data {
